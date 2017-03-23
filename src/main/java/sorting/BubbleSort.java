0bf5bf93d602d3485e944d166961cd12d9a417a8
Roberto Magale <roberto.magale@bbc.co.uk>
2017-03-23 13:40:48 +0000

    removed idea files

--- src/main/java/sorting/BubbleSort.java
+++ src/main/java/sorting/BubbleSort.java
@@ -1,5 +1,38 @@
 package sorting;
 
+/**
+ * Bubble Sort  is a simple sorting algorithm that repeatedly steps through the list to be sorted, compares each pair
+ * of adjacent items and swaps them if they are in the wrong order. The pass through the list is repeated until no
+ * swaps are needed, which indicates that the list is sorted. The algorithm, which is a comparison sort, is named for
+ * the way smaller or larger elements "bubble" to the top of the list. Although the algorithm is simple, it is too slow
+ * and impractical for most problems even when compared to insertion sort.[2] It can be practical if the input is
+ * usually in sorted order but may occasionally have some out-of-order elements nearly in position.
+ *
+ * Example:
+ *
+ * First Pass:
+ *  ( 5 1 4 2 8 ) –> ( 1 5 4 2 8 ), Here, algorithm compares the first two elements, and swaps since 5 > 1.
+ *  ( 1 5 4 2 8 ) –> ( 1 4 5 2 8 ), Swap since 5 > 4
+ *  ( 1 4 5 2 8 ) –> ( 1 4 2 5 8 ), Swap since 5 > 2
+ *  ( 1 4 2 5 8 ) –> ( 1 4 2 5 8 ), Now, since these elements are already in order (8 > 5), algorithm does not swap them.
+ *
+ * Second Pass:
+ *  ( 1 4 2 5 8 ) –> ( 1 4 2 5 8 )
+ *  ( 1 4 2 5 8 ) –> ( 1 2 4 5 8 ), Swap since 4 > 2
+ *  ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+ *  ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+ *
+ * Now, the array is already sorted, but our algorithm does not know if it is completed. The algorithm needs one whole
+ * pass without any swap to know it is sorted.
+ *
+ * Third Pass:
+ *  ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+ *  ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+ *  ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+ *  ( 1 2 4 5 8 ) –> ( 1 2 4 5 8 )
+ *
+ * <a href="https://www.tutorialspoint.com/data_structures_algorithms/bubble_sort_algorithm.htm">Link</a>
+ */
 public class BubbleSort {
 
     public int[] sort(int[] array) {
--- src/main/java/sorting/InsertionSort.java
+++ src/main/java/sorting/InsertionSort.java
@@ -1,5 +1,35 @@
 package sorting;
 
+/**
+ * Insertion sort is a simple sorting algorithm that builds the final sorted array (or list) one item at a time.
+ * It is much less efficient on large lists than more advanced algorithms such as quicksort, heapsort, or merge sort.
+ * However, insertion sort provides several advantages:
+ * <ul>
+ * <li>Simple implementation: Jon Bentley shows a three-line C version, and a five-line optimized version</li>
+ * <li>Efficient for (quite) small data sets, much like other quadratic sorting algorithms</li>
+ * <li>Milk</li>
+ * <li>Adaptive, i.e., efficient for data sets that are already substantially sorted: the time complexity is O(nk) when each element in the input is no more than k places away from its sorted position</li>
+ * <li>Stable; i.e., does not change the relative order of elements with equal keys</li>
+ * <li>In-place; i.e., only requires a constant amount O(1) of additional memory space</li>
+ * <li>Online; i.e., can sort a list as it receives it</li>
+ * </ul>
+ *
+ * Example:  The following table shows the steps for sorting the sequence 5 7 0 3 4 2 6 1. On the left side the sorted
+ * part of the sequence is shown in square brackets.
+ * For each iteration, the number of positions the inserted element has moved is shown in brackets.
+ * Altogether this amounts to 17 steps.
+ *
+ *   [5] 7	0	3	4	2	6	1	 	(0)
+ *   [5  7]	0	3	4	2	6	1	 	(0)
+ *   [0	 5	7]	3	4	2	6	1	 	(2)
+ *   [0	 3	5	7]	4	2	6	1	 	(2)
+ *   [0	3	4	5	7]	2	6	1	 	(2)
+ *   [0	2	3	4	5	7]	6	1	 	(4)
+ *   [0	2	3	4	5	6	7]	1	 	(1)
+ *   [0	1	2	3	4	5	6	7]	 	(6)
+ *
+ * <a href="https://www.tutorialspoint.com/data_structures_algorithms/insertion_sort_algorithm.htm">Link</a>
+ */
 public class InsertionSort {
 
     public int[] sort(int[] array) {
