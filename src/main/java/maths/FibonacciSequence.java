6b40aa905c3aaea0526b5aad26555d9200774cf9
Roberto Magale <roberto.magale@bbc.co.uk>
2017-04-16 08:34:26 +0100

    added dynamic programming to Fibonacci algorithm

--- src/main/java/maths/FibonacciSequence.java
+++ src/main/java/maths/FibonacciSequence.java
@@ -1,15 +1,21 @@
 package maths;
 
+import java.util.HashMap;
+
 /**
- *
  * Computes and prints the first n Fibonacci numbers.
- *
  */
 public class FibonacciSequence {
 
-    public long getValue(int index) {
+    /**
+     * Iterative version
+     *
+     * Time Complexity O(2^n)
+     */
+    public long getFibonacciValue(int index) {
         return fibonacci(index);
     }
+
 
     private long fibonacci(int index) {
         if (index < 2) {
@@ -17,5 +23,29 @@ public class FibonacciSequence {
         }
 
         return fibonacci(index - 1) + fibonacci(index - 2);
+    }
+
+    /**
+     * Dynamic programming version
+     *
+     * Time Complexity O(n)
+     *
+     * Space Complexity O(n)
+     */
+    public long getFibonaccyDynamicProgramming(int index) {
+        HashMap<Integer, Long> map = new HashMap<>();
+        if (index < 2) {
+            return 1;
+        }
+        map.put(0, 1L);
+        map.put(1, 1L);
+
+        for (int i = 2; i < index; i++) {
+            long oneBefore = map.get(i - 1);
+            long twoBefore = map.get(i - 2);
+            map.put(i, oneBefore + twoBefore);
+        }
+
+        return map.get(index - 1) + map.get(index - 2);
     }
 }
--- src/test/java/maths/FibonacciSequenceTest.java
+++ src/test/java/maths/FibonacciSequenceTest.java
@@ -2,7 +2,7 @@ package maths;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class FibonacciSequenceTest {
 
@@ -10,12 +10,23 @@ public class FibonacciSequenceTest {
 
     @Test
     public void shouldCalculateCorrectValue() {
-        assertEquals(1, sequence.getValue(0));
-        assertEquals(1, sequence.getValue(1));
-        assertEquals(2, sequence.getValue(2));
-        assertEquals(3, sequence.getValue(3));
-        assertEquals(5, sequence.getValue(4));
-        assertEquals(8, sequence.getValue(5));
-        assertEquals(13, sequence.getValue(6));
+        assertEquals(1, sequence.getFibonacciValue(0));
+        assertEquals(1, sequence.getFibonacciValue(1));
+        assertEquals(2, sequence.getFibonacciValue(2));
+        assertEquals(3, sequence.getFibonacciValue(3));
+        assertEquals(5, sequence.getFibonacciValue(4));
+        assertEquals(8, sequence.getFibonacciValue(5));
+        assertEquals(13, sequence.getFibonacciValue(6));
+    }
+
+    @Test
+    public void shouldCalculateCorrectValueWithDynamicProgrammingVersion() {
+        assertEquals(1, sequence.getFibonaccyDynamicProgramming(0));
+        assertEquals(1, sequence.getFibonaccyDynamicProgramming(1));
+        assertEquals(2, sequence.getFibonaccyDynamicProgramming(2));
+        assertEquals(3, sequence.getFibonaccyDynamicProgramming(3));
+        assertEquals(5, sequence.getFibonaccyDynamicProgramming(4));
+        assertEquals(8, sequence.getFibonaccyDynamicProgramming(5));
+        assertEquals(13, sequence.getFibonaccyDynamicProgramming(6));
     }
 }
